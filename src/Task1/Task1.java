0dd2833afc696da50a2a36068f8e4d722ad06617
Oleksandr Krasilovskyi <sashikkrasilovskyi@gmail.com>
2022-05-12 21:02:16 +0300

    List was changed into Stream

--- src/Task1/Task1.java
+++ src/Task1/Task1.java
@@ -1,14 +1,12 @@
 package Task1;
 
-import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Task1 {
-   public static void main(String[] args) {
-               
-       final var strings = List.of("Amazon", "Merge", "Kamaz", "Margo", "Amur", "Moroz", "Carrot");
+    public static void main(String[] args) {
 
-        strings.stream()
+        Stream.of("Amazon", "Merge", "Kamaz", "Margo", "Amur", "Moroz", "Carrot")
                 .filter(e -> e.startsWith("A") || e.endsWith("z"))
                 .collect(Collectors.toSet())
                 .forEach(System.out::println);
